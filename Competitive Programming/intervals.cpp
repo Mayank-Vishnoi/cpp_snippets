60bd11e898470c7ddf9fc19e30e11bdcf69adade
Mayank-Vishnoi <86844747+Mayank-Vishnoi@users.noreply.github.com>
2023-02-10 15:28:23 +0530

    Create intervals.cpp

---
+++ Competitive Programming/intervals.cpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Simon Lindholm
+ * Description: Add (merges overlapping intervals) and remove intervals from a set of disjoint intervals.
+ * Intervals are [inclusive, exclusive).
+ */
+
+set<pair<int, int>>::iterator addInterval(set<pair<int, int>>& is, int L, int R) {
+	if (L == R) return is.end();
+	auto it = is.lower_bound({L, R}), before = it;
+	while (it != is.end() && it->first <= R) {
+		R = max(R, it->second);
+		before = it = is.erase(it);
+	}
+	if (it != is.begin() && (--it)->second >= L) {
+		L = min(L, it->first);
+		R = max(R, it->second);
+		is.erase(it);
+	}
+	return is.insert(before, {L,R});
+}
+
+void removeInterval(set<pair<int, int>>& is, int L, int R) {
+	if (L == R) return;
+	auto it = addInterval(is, L, R);
+	auto r2 = it->second;
+	if (it->first == L) is.erase(it);
+	else (int&)it->second = L;
+	if (R != r2) is.emplace(R, r2);
+}
