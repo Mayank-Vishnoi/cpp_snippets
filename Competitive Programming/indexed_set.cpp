980e4c72813a1000c6b4315467a9fcd2ff3cc73f
Mayank-Vishnoi <86844747+Mayank-Vishnoi@users.noreply.github.com>
2023-05-04 18:50:05 +0530

    Update indexed_set.cpp

--- Competitive Programming/indexed_set.cpp
+++ Competitive Programming/indexed_set.cpp
@@ -7,3 +7,13 @@ cout << t.order_of_key(x) << endl;
 cout << *t.find_by_order(y) << endl;
 
 // Read more on https://codeforces.com/blog/entry/11080
+
+// Other applications: supporting elements less than x in a set
+struct left_right_array {
+   Tree<int> free;
+   explicit left_right_array (int n) { for (int i = 0; i < n; i++) free.insert(i); }
+   void set (int k) { free.erase(k); }
+   void unset (int k) { free.insert(k); }
+   int free_left (int i) { return (int)free.order_of_key(i + 1); }
+   int free_right (int i) { return (int)free.size() - free_left(i); }
+};
