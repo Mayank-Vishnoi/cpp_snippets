d96994a06c30b8db38c1f4928b7e72d5db60be3c
Mayank-Vishnoi <86844747+Mayank-Vishnoi@users.noreply.github.com>
2023-09-20 09:10:45 +0530

    Update seive_of_eratosthenes.cpp

--- Competitive Programming/seive_of_eratosthenes.cpp
+++ Competitive Programming/seive_of_eratosthenes.cpp
@@ -1,6 +1,6 @@
 const int N = 1e6;
-bitset<N + 1> p; 
-vector<int> pr;
+bitset<N + 1> p; // whether or not i is prime
+vector<int> pr; // list of primes
 
 void sieve () {
    p.flip();
@@ -12,4 +12,17 @@ void sieve () {
    for (int i = 1; i <= N; i++) 
       if (p[i]) 
          pr.push_back(i);
+}
+
+// smallest prime factor for each number
+int spf[N];
+void sieve() {
+   spf[1] = 1;
+   for (int i = 2; i < N; i++)
+      spf[i] = i;
+   for (int i = 2; i * i < N; i++)
+     if (spf[i] == i) 
+         for (int j = i * i; j < N; j += i)
+            if (spf[j] == j)
+               spf[j] = i;
 }
